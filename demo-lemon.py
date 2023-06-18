b00b0ac4f9bd97923b119d2037e6302239be0d86
Machine <68440743+Machinh@users.noreply.github.com>
2023-06-18 19:41:54 +0000

    Update demo-lemon.py

--- demo-lemon.py
+++ demo-lemon.py
@@ -1,10 +1,8 @@
 import string
 import numpy as np
-
-# Importar bibliotecas para salvar e carregar dados em formato JSON
 import json
 
-# Função para carregar os dados de treinamento de um arquivo
+# carregar os dados de treinamento de um arquivo
 def carregar_dados_arquivo(nome_arquivo, separador=';'):
     perguntas = []
     respostas = []
@@ -19,20 +17,20 @@ def carregar_dados_arquivo(nome_arquivo, separador=';'):
                 respostas.append(resposta)
     return perguntas, respostas
 
-# Função para salvar as perguntas e respostas em formato JSON
+# salvar as perguntas e respostas em formato JSON
 def salvar_dados_arquivo(nome_arquivo, perguntas, respostas):
     with open(nome_arquivo, 'w') as arquivo:
         for pergunta, resposta in zip(perguntas, respostas):
             linha = f"{pergunta};{resposta}\n"
             arquivo.write(linha)
 
-# Pré-processamento dos dados
+#processamento dos dados
 def preprocessamento(texto):
     texto = texto.lower()
     texto = texto.translate(str.maketrans('', '', string.punctuation))
     return texto
 
-# Função para calcular a similaridade cosseno entre duas strings
+# calcular a similaridade cosseno entre duas strings
 def similaridade_cosseno(str1, str2):
     palavras_str1 = set(str1.split())
     palavras_str2 = set(str2.split())
@@ -42,7 +40,7 @@ def similaridade_cosseno(str1, str2):
 
     return similaridade
 
-# Função para encontrar a pergunta mais similar
+# encontrar a pergunta mais similar
 def encontrar_pergunta_similar(pergunta, perguntas_preproc):
     perguntas_similares = []
     pergunta_preproc = preprocessamento(pergunta)
@@ -53,7 +51,7 @@ def encontrar_pergunta_similar(pergunta, perguntas_preproc):
     pergunta_similar_idx = perguntas_similares[0][0]
     return pergunta_similar_idx
 
-# Comandos para sair do loop
+# sair do loop
 comando_chave1 = {'sair', 'exit', 'quit'}
 
 # Solicitar nome do usuário
@@ -61,12 +59,12 @@ usuario = input('Qual é o seu nome? ')
 if usuario == '':
     usuario = 'Usuário'
 
-print(f'Olá {usuario}! O que temos para hoje?')
+print(f'Sessão Lemon iniciada usuário {usuario}, O quê temos para hoje?')
 
-# Carregar os dados de treinamento de um arquivo
+# Carregar dados de treinamento de um arquivo
 perguntas, respostas = carregar_dados_arquivo('memorias.txt')
 
-# Pré-processamento das perguntas
+# processamento das perguntas
 perguntas_preproc = [preprocessamento(pergunta) for pergunta in perguntas]
 
 # Loop infinito para responder a perguntas
@@ -80,12 +78,12 @@ while True:
     resposta_bot = respostas[pergunta_similar_idx]
     
     if similaridade_cosseno(preprocessamento(user_input), preprocessamento(resposta_bot)) < 0.4:
-        print('Lemon: Desculpe, eu não sei a resposta para essa pergunta. Você poderia me ensinar?')
+        print(f'Lemon: Desculpe usuário {usuario}, eu não sei a resposta para essa pergunta. Você poderia me ensinar?')
         nova_resposta = input('Sua resposta: ')
         perguntas.append(user_input)
         respostas.append(nova_resposta)
         perguntas_preproc.append(preprocessamento(user_input))
         salvar_dados_arquivo('memorias.txt', perguntas, respostas)
-        print('Lemon: Aprendi uma nova resposta! Obrigado por me ensinar.')
+        print(f'Lemon: Aprendi algo novo! Obrigado por me ensinar usuário {usuario}.')
     else:
         print('Lemon:', resposta_bot)
