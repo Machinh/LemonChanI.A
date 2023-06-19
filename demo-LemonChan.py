c020e5e29826af3388904492688024b9727c07d8
Machine <68440743+Machinh@users.noreply.github.com>
2023-06-19 01:12:55 +0000

    Update demo-LemonChan.py

--- demo-LemonChan.py
+++ demo-LemonChan.py
@@ -2,8 +2,9 @@ import string
 import numpy as np
 import json
 from unidecode import unidecode
+import subprocess
 
-# carregar os dados de treinamento de um arquivo
+# dados de treinamento de um arquivo
 def carregar_dados_arquivo(nome_arquivo, separador=';'):
     perguntas = []
     respostas = []
@@ -53,21 +54,47 @@ def encontrar_pergunta_similar(pergunta, perguntas_preproc):
     pergunta_similar_idx = perguntas_similares[0][0]
     return pergunta_similar_idx
 
+# algoritmo de aprendizado por reforço Q-Learning
+def q_learning(perguntas, respostas, perguntas_preproc, recompensas, taxa_aprendizado=0.1, fator_desconto=0.9, num_iteracoes=1000):
+    num_perguntas = len(perguntas)
+    q_values = np.zeros((num_perguntas, num_perguntas))
+
+    for _ in range(num_iteracoes):
+        pergunta_idx = np.random.randint(num_perguntas)
+        pergunta = perguntas[pergunta_idx]
+
+        pergunta_preproc = preprocessamento(pergunta)
+        pergunta_similar_idx = encontrar_pergunta_similar(pergunta, perguntas_preproc)
+        resposta_bot = respostas[pergunta_similar_idx]
+
+        if similaridade_cosseno(pergunta_preproc, preprocessamento(resposta_bot)) < 0.4:
+            q_values[pergunta_idx][pergunta_similar_idx] += taxa_aprendizado * (recompensas[pergunta_similar_idx] + fator_desconto * np.max(q_values[pergunta_similar_idx]) - q_values[pergunta_idx][pergunta_similar_idx])
+        else:
+            q_values[pergunta_idx][pergunta_similar_idx] += taxa_aprendizado * (recompensas[pergunta_similar_idx] - q_values[pergunta_idx][pergunta_similar_idx])
+
+    return q_values
+
 # sair do loop
 comando_chave1 = {'sair', 'exit', 'quit'}
 
-# Solicitar nome do usuário
+# nome do usuário
 usuario = input('Qual é o seu nome? ')
 if usuario == '':
     usuario = 'Usuário'
 
-print(f'Sessão Lemon iniciada usuário {usuario}, O quê temos para hoje?')
+print(f'Sessão Lemon iniciando usuário {usuario}, O quê temos para hoje?')
 
-# Carregar dados de treinamento de um arquivo
+# dados de treinamento do arquivo
 perguntas, respostas = carregar_dados_arquivo('memorias.txt')
 
 # processamento das perguntas
 perguntas_preproc = [preprocessamento(pergunta) for pergunta in perguntas]
+
+# Definir recompensas
+recompensas = np.zeros(len(perguntas))
+
+# algoritmo de Q-Learning
+q_values = q_learning(perguntas, respostas, perguntas_preproc, recompensas)
 
 # Loop infinito para responder a perguntas
 while True:
@@ -76,16 +103,42 @@ while True:
     if user_input.lower() in comando_chave1:
         break
 
-    pergunta_similar_idx = encontrar_pergunta_similar(user_input, perguntas_preproc)
-    resposta_bot = respostas[pergunta_similar_idx]
-    
-    if similaridade_cosseno(preprocessamento(user_input), preprocessamento(resposta_bot)) < 0.4:
-        print(f'Lemon: Desculpe usuário {usuario}, eu não sei a resposta para essa pergunta. Você poderia me ensinar?')
-        nova_resposta = input('Sua resposta: ')
-        perguntas.append(user_input)
-        respostas.append(nova_resposta)
-        perguntas_preproc.append(preprocessamento(user_input))
-        salvar_dados_arquivo('memorias.txt', perguntas, respostas)
-        print(f'Lemon: Aprendi algo novo! Obrigado por me ensinar usuário {usuario}.')
+    if user_input.lower() == 'qual é o meu ip?':
+        # comando para obter o endereço IP
+        resultado = subprocess.check_output(['curl', 'ifconfig.me'])
+
+        # resultado em uma string legível
+        resultado_str = resultado.decode('utf-8').strip()
+
+        # resultado para o usuário
+        print(f'Lemon: Seu endereço IP é: {resultado_str}')
+
+    elif user_input.lower().startswith('use o nmap'):
+        # endereço IP da pergunta do usuário
+        ip = user_input.split('use o nmap para escanear ')[1]
+
+        # comando Nmap usando o endereço IP
+        resultado = subprocess.check_output(['nmap', ip])
+
+        # resultado em uma string legível
+        resultado_str = resultado.decode('utf-8')
+
+        # resultado para o usuário
+        print(f'Lemon: Aqui está o resultado da varredura da rede:\n{resultado_str}')
     else:
-        print('Lemon:', resposta_bot)
+        pergunta_similar_idx = encontrar_pergunta_similar(user_input, perguntas_preproc)
+        resposta_bot = respostas[pergunta_similar_idx]
+
+        if similaridade_cosseno(preprocessamento(user_input), preprocessamento(resposta_bot)) < 0.2:
+            print(f'Lemon: Desculpe usuário {usuario}, eu não sei a resposta para essa pergunta. Você poderia me ensinar detalhadamente?')
+            nova_resposta = input('Sua resposta: ')
+            perguntas.append(user_input)
+            respostas.append(nova_resposta)
+            perguntas_preproc.append(preprocessamento(user_input))
+            recompensas = np.append(recompensas, 1.0)  # recompensa positiva para a nova resposta
+            q_values = q_learning(perguntas, respostas, perguntas_preproc, recompensas)
+            salvar_dados_arquivo('memorias.txt', perguntas, respostas)
+            print(f'Lemon: Aprendi algo novo! Obrigado por me ensinar usuário {usuario}.')
+        else:
+            print('Lemon:', resposta_bot)
+            recompensas[pergunta_similar_idx] = 1.0  # recompensa positiva para resposta correta
